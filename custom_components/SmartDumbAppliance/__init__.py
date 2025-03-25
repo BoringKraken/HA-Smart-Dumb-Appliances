f5074ecb8c1c3caaab72fa92e651264eda20bcf6
BoringKraken <88515180+BoringKraken@users.noreply.github.com>
2025-03-26 09:47:29 +1100

    Update __init__.py
    used wrong call i think.

--- custom_components/SmartDumbAppliance/__init__.py
+++ custom_components/SmartDumbAppliance/__init__.py
@@ -1,26 +1,32 @@
 import logging
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
-
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the Smart Dumb Appliance Integration."""
+    hass.data.setdefault(DOMAIN, {})
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Smart Dumb Appliance from a config entry."""
-
+    
     # Store a reference to the configuration entry
-    hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry
 
-    # Forward setup to platform
-    hass.config_entries.async_setup_platforms(entry, ["sensor"])
+    # Forward setup to the sensor platform
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
 
     _LOGGER.debug("Entry setup: %s", entry.data)
     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
