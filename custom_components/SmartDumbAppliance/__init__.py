1b9c46d18c47e624f8ff5cbf2e76c34a011f17da
BoringKraken <88515180+BoringKraken@users.noreply.github.com>
2025-03-25 14:44:12 +1100

    Update __init__.py
    changed to have Cost per KwH as a HA Helper

--- custom_components/SmartDumbAppliance/__init__.py
+++ custom_components/SmartDumbAppliance/__init__.py
@@ -1,19 +1,19 @@
 from homeassistant.core import HomeAssistant
-from .const import DOMAIN  # Import the domain from the constants
+from .const import DOMAIN
 
 async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the integration from configuration.yaml if needed."""
-    hass.data.setdefault(DOMAIN, {})  # Initialize domain data storage
-    return True  # Return True to indicate successful setup
+    """Set up smart dumb appliance from configuration.yaml."""
+    hass.data.setdefault(DOMAIN, {})
+    return True
 
 async def async_setup_entry(hass: HomeAssistant, entry):
-    """Set up the smart dumb appliance from a config entry."""
+    """Set up smart dumb appliance from a config entry."""
     # Store entry data in Home Assistant's domain-specific data storage
     hass.data[DOMAIN][entry.entry_id] = entry.data
-    return True  # Return True to indicate successful entry setup
+    return True
 
 async def async_unload_entry(hass: HomeAssistant, entry):
     """Unload a config entry."""
     # Remove entry data from Home Assistant's domain-specific storage
     hass.data[DOMAIN].pop(entry.entry_id)
-    return True  # Return True to indicate successful entry unload
+    return True
