4ad53429345e056e06c1801dd300253c06b86a83
BoringKraken <88515180+BoringKraken@users.noreply.github.com>
2025-03-26 14:40:19 +1100

    Update sensor.py

--- custom_components/SmartDumbAppliance/sensor.py
+++ custom_components/SmartDumbAppliance/sensor.py
@@ -1,72 +1,87 @@
+"""
+Implements sensor entities for Smart Dumb Appliance.
+Example: A cost sensor that calculates cost by multiplying usage * cost/kWh.
+"""
+
 import logging
-from datetime import datetime
 from homeassistant.components.sensor import SensorEntity
-from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.device_registry import DeviceEntryType
+from homeassistant.const import STATE_UNKNOWN, STATE_UNAVAILABLE
+
+from .const import DOMAIN, CONF_POWER_SENSOR, CONF_COST_SENSOR, CONF_DEVICE_NAME
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry, async_add_entities):
-    """Set up Smart Dumb Appliance sensors through config entry."""
-    entry_data_list = config_entry.data.get("devices", [])
-    _LOGGER.debug("Config entry data received: %s", entry_data_list)
-    
-    sensors = []
+async def async_setup_entry(hass: HomeAssistant, entry, async_add_entities):
+    """Set up Smart Dumb Appliance sensors from a ConfigEntry."""
+    config = hass.data[DOMAIN][entry.entry_id]
+    device_name = config.get(CONF_DEVICE_NAME, "Appliance")
+    power_sensor_id = config.get(CONF_POWER_SENSOR)
+    cost_sensor_id = config.get(CONF_COST_SENSOR)
 
-    for entry_data in entry_data_list:
-        _LOGGER.debug("Processing device data: %s", entry_data)
-        # Creating a sensor for each device entry
-        appliance_sensor = ApplianceSensor(hass, entry_data)
-        _LOGGER.debug("Created appliance sensor: %s", appliance_sensor)
-        sensors.append(appliance_sensor)
-    
+    sensors = []
+    if cost_sensor_id:
+        sensors.append(
+            SmartDumbApplianceCostSensor(
+                entry_id=entry.entry_id,
+                name=f"{device_name} Cost",
+                power_sensor_entity_id=power_sensor_id,
+                cost_sensor_entity_id=cost_sensor_id,
+            )
+        )
+
     async_add_entities(sensors, update_before_add=True)
 
-class ApplianceSensor(SensorEntity):
-    """Representation of a sensor for monitoring appliance state."""
+class SmartDumbApplianceCostSensor(SensorEntity):
+    """A sensor that computes cost from a power sensor and a cost-per-kWh sensor."""
 
-    def __init__(self, hass, entry_data):
-        """Initialize the appliance sensor."""
-        self._hass = hass
-        self._entry_data = entry_data
-        self._name = entry_data.get("name", "Unknown Appliance")
-        self._state = "idle"
-        self._extra_attributes = {}
+    def __init__(self, entry_id, name, power_sensor_entity_id, cost_sensor_entity_id):
+        self._entry_id = entry_id
+        self._name = name
+        self._power_sensor_entity_id = power_sensor_entity_id
+        self._cost_sensor_entity_id = cost_sensor_entity_id
+        self._state = None
+        self._attr_unique_id = f"{entry_id}_cost_sensor"
 
     @property
     def name(self):
-        """Return the name of the appliance sensor."""
+        """Name shown in the UI."""
         return self._name
 
     @property
-    def unique_id(self):
-        """Return a unique ID for this sensor."""
-        return f"{self._entry_data.get('name')}_sensor"
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
+    def native_value(self):
+        """The calculated cost."""
         return self._state
 
     @property
-    def extra_state_attributes(self):
-        """Return additional state attributes."""
-        return self._extra_attributes
+    def native_unit_of_measurement(self):
+        """Return a suitable currency symbol or code."""
+        return "USD"
 
     @property
-    def device_info(self):
-        """Return device info for this entity."""
-        return {
-            "identifiers": {(self._hass.components.smart_dumb_appliance.DOMAIN, self.unique_id)},
-            "name": self._name,
-            "manufacturer": "HA Smart Dumb Appliance",
-            "model": self._entry_data.get('model', 'Model XYZ'),
-            "sw_version": self._entry_data.get('sw_version', '1.0'),
-            "area_id": self._entry_data.get('area_id'),  # Example of setting a custom area
-            "via_device": (self._hass.components.smart_dumb_appliance.DOMAIN, self._entry_data.get('via_device'))
-        }
+    def should_poll(self):
+        """We'll poll the state machine for updates."""
+        return True
 
     async def async_update(self):
-        """Fetch new state data for the sensor."""
-        self._extra_attributes.update({"last_updated": datetime.utcnow().isoformat()})
+        """Fetch usage and cost, then calculate total cost."""
+        power_state_obj = self.hass.states.get(self._power_sensor_entity_id)
+        cost_state_obj = self.hass.states.get(self._cost_sensor_entity_id)
+
+        if not power_state_obj or power_state_obj.state in (STATE_UNKNOWN, STATE_UNAVAILABLE):
+            usage_kwh = 0
+        else:
+            try:
+                usage_kwh = float(power_state_obj.state)
+            except ValueError:
+                usage_kwh = 0
+
+        if not cost_state_obj or cost_state_obj.state in (STATE_UNKNOWN, STATE_UNAVAILABLE):
+            cost_per_kwh = 0
+        else:
+            try:
+                cost_per_kwh = float(cost_state_obj.state)
+            except ValueError:
+                cost_per_kwh = 0
+
+        self._state = usage_kwh * cost_per_kwh
