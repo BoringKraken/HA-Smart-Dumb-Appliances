f661b79e84f845b46c4f440126fcab147a7b0a8c
BoringKraken <88515180+BoringKraken@users.noreply.github.com>
2025-03-26 14:39:37 +1100

    Update const.py

--- custom_components/SmartDumbAppliance/const.py
+++ custom_components/SmartDumbAppliance/const.py
@@ -1,2 +1,15 @@
-# Define the domain of the integration for Home Assistant
+"""
+Stores all constants for the Smart Dumb Appliance integration.
+"""
+
 DOMAIN = "smart_dumb_appliance"
+DEFAULT_NAME = "Smart Dumb Appliance"
+
+CONF_POWER_SENSOR = "power_sensor"
+CONF_COST_SENSOR = "cost_sensor"
+CONF_DEAD_ZONE = "dead_zone"
+CONF_DEBOUNCE = "debounce"
+CONF_DEVICE_NAME = "device_name"
+
+ATTR_START_TIME = "start_time"
+ATTR_END_TIME = "end_time"
